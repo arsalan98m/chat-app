7c7b97b9db2607dc4c7680ddc66128bbf45a8b17
arsalan manzoor <arsalan1998m@gmail.com>
2021-02-07 22:39:53 +0500

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import "rsuite/dist/styles/rsuite-default.css";
 import "./styles/main.css";
+import { Switch, Route } from "react-router-dom";
+
+//Pages
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+
+// components
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
-  return <div className="text-blue">Testing</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
